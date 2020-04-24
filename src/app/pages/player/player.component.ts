e366dbc4b3a4b9b9be6c8aba2a9256b4a38c9f4a
Steve Black <stevenjblack@gmail.com>
2020-04-24 16:12:14 -0400

    Refactor and introduced `levelExplanationObserver`

--- src/app/pages/player/player.component.ts
+++ src/app/pages/player/player.component.ts
@@ -1,6 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, combineLatest } from 'rxjs';
 import { distinctUntilChanged, map } from 'rxjs/operators';
+
+const xpLevelValues = [
+  0,
+  300,
+  900,
+  2700,
+  6500,
+  14000,
+  23000,
+  34000,
+  48000,
+  64000,
+  85000,
+  100000,
+  120000,
+  140000,
+  165000,
+  195000,
+  225000,
+  265000,
+  305000,
+  355000,
+];
 
 @Component({
   selector: 'app-player',
@@ -18,33 +41,24 @@ export class PlayerComponent implements OnInit {
   }
   readonly levelObserver = this.xpObserver.pipe(
     map((xp) => {
-      const xpLevelValues = [
-        0,
-        300,
-        900,
-        2700,
-        6500,
-        14000,
-        23000,
-        34000,
-        48000,
-        64000,
-        85000,
-        100000,
-        120000,
-        140000,
-        165000,
-        195000,
-        225000,
-        265000,
-        305000,
-        355000,
-      ];
       let level = 0;
 
       while (xp >= xpLevelValues[++level]) {}
 
       return level;
+    }),
+    distinctUntilChanged(),
+  );
+  readonly levelExplanationObserver = combineLatest(this.xpObserver, this.levelObserver).pipe(
+    map(([xp, level]) => {
+      if (level === 20) {
+        return `You maxed out at level 20 with ${xpLevelValues[xpLevelValues.length - 1]}xp!`;
+      }
+
+      const nextXp = xpLevelValues[level];
+      const delta = nextXp - xp;
+
+      return `You will get to level ${level + 1} when you have ${nextXp}xp.\nOnly ${delta} more to go!`;
     }),
   );
 
